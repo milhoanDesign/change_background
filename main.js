55bb42a14ad8fa8836e4e961f19beeb1199eaabb
Walker Milhoan <walker@milhoandesign.com>
2024-04-16 12:16:48 -0600

    Changed some HTML

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 
     function getRandomColor(){
         
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-        }
-        return color;
+            let letters = '0123456789ABCDEF';
+            let color = '#';
+            
+            for (let i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
         };
+        
         function changeColor(){
-        let newColor = getRandomColor();
-        document.body.style.backgroundColor = newColor;
-    }; 
+        
+            let newColor = getRandomColor();
+            document.body.style.backgroundColor = newColor;
+        }; 
